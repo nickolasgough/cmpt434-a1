aed79297817ab5e4f0447b21b873a3680f18cd21
Nickolas Gough <nickolas.v.gough@gmail.com>
2019-01-16 15:02:42 -0600

    Try something.

--- udp-server.c
+++ udp-server.c
@@ -83,18 +83,16 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
 
-    rSize = recvfrom(hostFd, message, INPUT_MAX, 0, (struct sockaddr*) &clientAddr, &clientLen);
-    if (rSize == -1) {
-        printf("udp-server: failed to receive command from client\n");
-        exit(1);
-    }
+    while (1) {
+        rSize = recvfrom(hostFd, message, INPUT_MAX, 0, (struct sockaddr*) &clientAddr, &clientLen);
+        if (rSize == -1) {
+            printf("udp-server: failed to receive command from client\n");
+            exit(1);
+        }
 
-    get_file(hostFd, clientAddr, clientLen);
-
-    if (sendto(hostFd, "ready", INPUT_MAX, 0, (struct sockaddr*) &clientAddr, clientLen) == - 1) {
-        printf("udp-server: failed to send get file name response\n");
-        printf("Error: %d - %s\n", errno, strerror(errno));
-        return 0;
+        if (strcmp(message, "get") == 0) {
+            get_file(hostFd, clientAddr, clientLen);
+        }
     }
 
 
