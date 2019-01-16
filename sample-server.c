70e29a82867034b11998dc92d37c73b860267312
Nickolas Gough <nickolas.v.gough@gmail.com>
2019-01-16 12:49:06 -0600

    Update sample.

--- sample-server.c
+++ sample-server.c
@@ -1,55 +1,72 @@
-// Server side implementation of UDP client-server model 
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <unistd.h> 
-#include <string.h> 
-#include <sys/types.h> 
-#include <sys/socket.h> 
-#include <arpa/inet.h> 
-#include <netinet/in.h> 
-  
-#define PORT     8080 
-#define MAXLINE 1024 
-  
-// Driver code 
-int main() { 
-    int sockfd; 
-    char buffer[MAXLINE]; 
-    char *hello = "Hello from server"; 
-    struct sockaddr_in servaddr, cliaddr; 
-      
-    // Creating socket file descriptor 
-    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
-        perror("socket creation failed"); 
-        exit(EXIT_FAILURE); 
-    } 
-      
-    memset(&servaddr, 0, sizeof(servaddr)); 
-    memset(&cliaddr, 0, sizeof(cliaddr)); 
-      
-    // Filling server information 
-    servaddr.sin_family    = AF_INET; // IPv4 
-    servaddr.sin_addr.s_addr = INADDR_ANY; 
-    servaddr.sin_port = htons(PORT); 
-      
-    // Bind the socket with the server address 
-    if ( bind(sockfd, (const struct sockaddr *)&servaddr,  
-            sizeof(servaddr)) < 0 ) 
-    { 
-        perror("bind failed"); 
-        exit(EXIT_FAILURE); 
-    } 
-      
-    int len, n; 
-    n = recvfrom(sockfd, (char *)buffer, MAXLINE,  
-                MSG_WAITALL, ( struct sockaddr *) &cliaddr, 
-                &len); 
-    buffer[n] = '\0'; 
-    printf("Client : %s\n", buffer); 
-    sendto(sockfd, (const char *)hello, strlen(hello),  
-        MSG_CONFIRM, (const struct sockaddr *) &cliaddr, 
-            len); 
-    printf("Hello message sent.\n");  
-      
-    return 0; 
+/* Nickolas Gough, nvg081, 11181823 */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <errno.h>
+
+#include "x-sockets.h"
+#include "x-common.h"
+
+
+int main(int argc, char* argv[]) {
+    char* hName;
+    char* hPort;
+    int hostFd;
+    struct addrinfo hostInfo;
+    struct sockaddr_storage clientAddr;
+    socklen_t clientLen;
+    char* message;
+
+    hPort = argv[1];
+    if (!check_port(hPort)) {
+        printf("udp-server: port number must be between 30000 and 40000\n");
+        exit(1);
+    }
+
+    hName = calloc(INPUT_MAX, sizeof(char));
+    if (hName == NULL) {
+        printf("udp-server: failed to allocate necessary memory\n");
+        exit(1);
+    }
+
+    if (gethostname(hName, INPUT_MAX) == -1) {
+        printf("udp-server: failed to determine the name of the machine\n");
+        exit(1);
+    }
+    if (!udp_socket(&hostFd, &hostInfo, hName, hPort)) {
+        printf("udp-server: failed to create udp socket for given host\n");
+        exit(1);
+    }
+
+    if (bind(hostFd, hostInfo.ai_addr, hostInfo.ai_addrlen) == -1) {
+        printf("udp-server: failed to bind udp socket for given host\n");
+        exit(1);
+    }
+
+    message = calloc(INPUT_MAX, sizeof(char));
+    if (message == NULL) {
+        printf("udp-server: failed to allocate necessary memory\n");
+        exit(1);
+    }
+
+    if (recvfrom(hostFd, message, INPUT_MAX, 0, (struct sockaddr*) &clientAddr, &clientLen) == -1) {
+        printf("udp-server: failed to receive from client\n");
+        exit(1);
+    }
+    printf("%s\n", message);
+    if (sendto(hostFd, "hello", INPUT_MAX, 0, (struct sockaddr*) &clientAddr, clientLen) == -1) {
+        printf("udp-server: failed to reply to client\n");
+        printf("Error: %d - %s\n", errno, strerror(errno));
+        exit(1);
+    }
+
+    close(hostFd);
+    exit(0);
 }
