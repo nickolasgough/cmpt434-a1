f1c9cffc237df2b20191742092de12f3ad656dc7
Nickolas Gough <nickolas.v.gough@gmail.com>
2019-01-16 11:17:10 -0600

    Try a simple example.

--- udp-server.c
+++ udp-server.c
@@ -14,116 +14,17 @@
 #include "x-common.h"
 
 
-void get_file(int hostFd, int clientFd, struct sockaddr clientAddr, socklen_t clientLen) {
-    char* message;
-    char* fName;
-
-    message = calloc(INPUT_MAX, sizeof(char));
-    fName = calloc(INPUT_MAX, sizeof(char));
-    if (message == NULL || fName == NULL) {
-        return;
-    }
-
-    sprintf(message, "%s", "ready");
-    if (sendto(clientFd, message, INPUT_MAX, 0, &clientAddr, clientLen) == - 1) {
-        printf("udp-server: failed to send get file name response\n");
-        return;
-    }
-    memset(message, 0, INPUT_MAX);
-
-    if (recvfrom(hostFd, fName, INPUT_MAX, 0, NULL, NULL) == -1) {
-        printf("udp-server: failed to receive get file name\n");
-        return;
-    }
-
-    if (access(fName, F_OK)) {
-        printf("udp-server: file %s does not exist\n", fName);
-        sprintf(message, "%s", "error");
-        if (sendto(clientFd, message, INPUT_MAX, 0, &clientAddr, clientLen) == -1) {
-            printf("udp-server: failed to send file does not exist error\n");
-        }
-        return;
-    }
-    memset(message, 0, INPUT_MAX);
-
-    sprintf(message, "%s", "ready");
-    if (sendto(clientFd, message, INPUT_MAX, 0, &clientAddr, clientLen) == -1) {
-        printf("udp-server: failed to send get file name response\n");
-        return;
-    }
-    memset(message, 0, INPUT_MAX);
-
-    udp_file_transmit("udp-server", hostFd, clientFd, fName, clientAddr, clientLen);
-}
-
-
-void put_file(int hostFd, int clientFd, struct sockaddr clientAddr, socklen_t clientLen)  {
-    char* message;
-    char* fName;
-
-    message = calloc(INPUT_MAX, sizeof(char));
-    fName = calloc(INPUT_MAX, sizeof(char));
-    if (message == NULL || fName == NULL) {
-        printf("udp-server: failed to allocate necessary memory\n");
-        return;
-    }
-
-    sprintf(message, "%s", "ready");
-    if (sendto(clientFd, message, INPUT_MAX, 0, &clientAddr, clientLen) == - 1) {
-        printf("udp-server: failed to send put file name response\n");
-        return;
-    }
-    memset(message, 0, INPUT_MAX);
-
-    if (recvfrom(hostFd, fName, INPUT_MAX, 0, NULL, NULL) == -1) {
-        printf("udp-server: failed to receive put file name\n");
-        return;
-    }
-
-    if (!access(fName, F_OK)) {
-        printf("udp-server: file %s already exists\n", fName);
-        sprintf(message, "%s", "error");
-        if (sendto(clientFd, message, INPUT_MAX, 0, &clientAddr, clientLen) == -1) {
-            printf("udp-server: failed to send file exists error\n");
-        }
-        return;
-    }
-    memset(message, 0, INPUT_MAX);
-
-    sprintf(message, "%s", "ready");
-    if (sendto(clientFd, message, INPUT_MAX, 0, &clientAddr, clientLen) == - 1) {
-        printf("udp-server: failed to send put file name response\n");
-        return;
-    }
-    memset(message, 0, INPUT_MAX);
-
-    udp_file_receive("udp-server", hostFd, clientFd, fName, clientAddr, clientLen);
-}
-
-
 int main(int argc, char* argv[]) {
     char* hName;
     char* hPort;
-    char* sName;
-    char* sPort;
     int hostFd;
     struct addrinfo hostInfo;
-    int clientFd;
-    struct addrinfo clientInfo;
     struct sockaddr_in clientAddr;
     socklen_t clientLen;
     char* message;
-    int rSize;
-
-    if (argc != 4) {
-        printf("usage: ./udp-server <host port> <client name> <client port>\n");
-        exit(1);
-    }
 
     hPort = argv[1];
-    sName = argv[2];
-    sPort = argv[3];
-    if (!check_port(hPort) || !check_port(sPort)) {
+    if (!check_port(hPort)) {
         printf("udp-server: port number must be between 30000 and 40000\n");
         exit(1);
     }
@@ -148,35 +49,19 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
 
-    if (!udp_socket(&clientFd, &clientInfo, sName, sPort)) {
-        printf("udp-server: failed to create udp socket for given client\n");
-        exit(1);
-    }
-
     message = calloc(INPUT_MAX, sizeof(char));
     if (message == NULL) {
         printf("udp-server: failed to allocate necessary memory\n");
         exit(1);
     }
 
-    while (1) {
-        rSize = recvfrom(hostFd, message, INPUT_MAX, 0, (struct sockaddr*) &clientAddr, &clientLen);
-        if (rSize == -1) {
-            printf("udp-server: failed to receive command from client\n");
-            exit(1);
-        }
-        sprintf(message, "%s", "ready");
-        if (sendto(hostFd, message, INPUT_MAX, 0, (struct sockaddr*) &clientAddr, clientLen) >= 0) {
-            printf("successfully sent reply\n");
-        }
-        memset(message, 0, INPUT_MAX);
-
-        if (strcmp(message, "get") == 0) {
-            get_file(hostFd, clientFd, *clientInfo.ai_addr, clientInfo.ai_addrlen);
-        }
-        if (strcmp(message, "put") == 0) {
-            put_file(hostFd, clientFd, *clientInfo.ai_addr, clientInfo.ai_addrlen);
-        }
+    if (recvfrom(hostFd, message, INPUT_MAX, 0, (struct sockaddr*) &clientAddr, &clientLen) == -1) {
+        printf("udp-server: failed to receive from client\n");
+        exit(1);
+    }
+    if (sendto(hostFd, message, INPUT_MAX, 0, (struct sockaddr*) &clientAddr, clientLen) == -1) {
+        printf("udp-server: failed to reply to client\n");
+        exit(1);
     }
 
     close(hostFd);
--- x-client.c
+++ x-client.c
@@ -14,167 +14,43 @@
 #include "x-common.h"
 
 
-void get_file(int serverFd, char* lFile, char* rFile) {
-    char* message;
-
-    if (!access(lFile, F_OK)) {
-        printf("x-client: file %s already exists\n", lFile);
-        return;
-    }
-
-    message = calloc(INPUT_MAX, sizeof(char));
-    if (message == NULL) {
-        printf("x-client: failed to allocate necessary memory\n");
-        return;
-    }
-
-    sprintf(message, "%s", "get");
-    if (send(serverFd, message, INPUT_MAX, 0) == -1) {
-        printf("x-client: failed to transmit the get command\n");
-        return;
-    }
-    if (recv(serverFd, message, INPUT_MAX, 0) == -1) {
-        printf("x-client: failed to receive get command response\n");
-        return;
-    }
-    if (strcmp(message, "ready") != 0) {
-        printf("x-client: received unexpected get command response\n");
-        return;
-    }
-    memset(message, 0, INPUT_MAX);
-
-    if (send(serverFd, rFile, INPUT_MAX, 0) == -1) {
-        printf("x-client: failed to transmit the get file name\n");
-        return;
-    }
-    if (recv(serverFd, message, INPUT_MAX, 0) == -1) {
-        printf("x-client: failed to receive get file name response\n");
-        return;
-    }
-    if (strcmp(message, "ready") != 0) {
-        printf("x-client: received unexpected get file name response\n");
-        return;
-    }
-    memset(message, 0, INPUT_MAX);
-
-    tcp_file_receive("x-client", serverFd, lFile);
-}
-
-
-void put_file(int serverFd, char* lFile, char* rFile) {
-    char* message;
-    FILE* fPtr;
-
-    if (access(lFile, F_OK)) {
-        printf("x-client: file %s does not exist\n", lFile);
-        return;
-    }
-
-    message = calloc(INPUT_MAX, sizeof(char));
-    if (message == NULL) {
-        printf("x-client: failed to allocate necessary memory\n");
-        return;
-    }
-
-    fPtr = fopen(lFile, "r");
-    if (fPtr == NULL) {
-        printf("x-client: failed to open file %s", lFile);
-        return;
-    }
-
-    sprintf(message, "%s", "put");
-    if (send(serverFd, message, INPUT_MAX, 0) == -1) {
-        printf("x-client: failed to transmit the put command\n");
-        return;
-    }
-    if (recv(serverFd, message, INPUT_MAX, 0) == -1) {
-        printf("x-client: failed to receive put command response\n");
-        return;
-    }
-    if (strcmp(message, "ready") != 0) {
-        printf("x-client: received unexpected put command response\n");
-        return;
-    }
-    memset(message, 0, INPUT_MAX);
-
-    if (send(serverFd, rFile, INPUT_MAX, 0) == -1) {
-        printf("x-client: failed to transmit the put file name\n");
-        return;
-    }
-    if (recv(serverFd, message, INPUT_MAX, 0) == -1) {
-        printf("x-client: failed to receive put file name response\n");
-        return;
-    }
-    if (strcmp(message, "ready") != 0) {
-        printf("x-client: received unexpected put file name response\n");
-        return;
-    }
-    memset(message, 0, INPUT_MAX);
-
-    tcp_file_transmit("x-client", serverFd, lFile);
-}
-
-
 int main(int argc, char* argv[]) {
-    char* cmd;
-    char* lFile;
-    char* rFile;
-    char* hName;
-    char* port;
+    char* message;
+    char* sName;
+    char* sPort;
     int serverFd;
-    struct addrinfo sockInfo;
+    struct addrinfo serverInfo;
 
     if (argc != 3) {
         printf("usage: ./x-client <host name> <port number>\n");
         exit(1);
     }
 
-    hName = argv[1];
-    port = argv[2];
-    if (!check_port(port)) {
+    sName = argv[1];
+    sPort = argv[2];
+    if (!check_port(sPort)) {
         printf("x-client: port number must be between 30000 and 40000\n");
         exit(1);
     }
 
-    if (!tcp_socket(&serverFd, &sockInfo, hName, port)) {
-        printf("x-client: failed to create tcp socket for given host\n");
-        exit(1);
-    }
-    if (connect(serverFd, sockInfo.ai_addr, sockInfo.ai_addrlen) == -1) {
-        printf("x-client: failed to connect tcp socket for given host\n");
+    if (!udp_socket(&serverFd, &serverInfo, sName, sPort)) {
+        printf("x-client: failed to create udp socket for given host\n");
         exit(1);
     }
 
-    cmd = calloc(INPUT_MAX, sizeof(char));
-    lFile = calloc(INPUT_MAX, sizeof(char));
-    rFile = calloc(INPUT_MAX, sizeof(char));
-    if (cmd == NULL || rFile == NULL || lFile == NULL) {
+    message = calloc(INPUT_MAX, sizeof(char));
+    if (message == NULL) {
         printf("x-client: failed to allocate necessary memory\n");
         exit(1);
     }
 
-    while (1) {
-        printf("x-client? ");
-
-        scanf("%s", cmd);
-        if (strcmp(cmd, "quit") == 0) {
-            exit(0);
-        }
-
-        scanf("%s %s", lFile, rFile);
-        if (strcmp(cmd, "get") == 0) {
-            get_file(serverFd, lFile, rFile);
-        }
-        else if (strcmp(cmd, "put") == 0) {
-            put_file(serverFd, lFile, rFile);
-        }
-        else {
-            printf("Unknown command.\n");
-            printf("Known commands:\n");
-            printf("get <local file> <remote file>\n");
-            printf("put <local file> <remote file>\n");
-            printf("quit\n");
-        }
+    if (sendto(serverFd, message, INPUT_MAX, 0, (struct sockaddr*) serverInfo.ai_addr, serverInfo.ai_addrlen) == -1) {
+        printf("x-client: failed to send to server\n");
+        exit(1);
+    }
+    if (recvfrom(serverFd, message, INPUT_MAX, 0, (struct sockaddr*) NULL, NULL) == -1) {
+        printf("x-client: failed to receive from server\n");
+        exit(1);
     }
 
     close(serverFd);
