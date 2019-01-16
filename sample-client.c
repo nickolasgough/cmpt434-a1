491373a60eac32bc34a69eaca5a5bff15ffc8bfa
Nickolas Gough <nickolas.v.gough@gmail.com>
2019-01-16 12:48:35 -0600

    Update sample.

--- sample-client.c
+++ sample-client.c
@@ -1,49 +1,59 @@
-// Client side implementation of UDP client-server model 
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <unistd.h> 
-#include <string.h> 
-#include <sys/types.h> 
-#include <sys/socket.h> 
-#include <arpa/inet.h> 
-#include <netinet/in.h> 
-  
-#define PORT     8080 
-#define MAXLINE 1024 
-  
-// Driver code 
-int main() { 
-    int sockfd; 
-    char buffer[MAXLINE]; 
-    char *hello = "Hello from client"; 
-    struct sockaddr_in     servaddr; 
-  
-    // Creating socket file descriptor 
-    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
-        perror("socket creation failed"); 
-        exit(EXIT_FAILURE); 
-    } 
-  
-    memset(&servaddr, 0, sizeof(servaddr)); 
-      
-    // Filling server information 
-    servaddr.sin_family = AF_INET; 
-    servaddr.sin_port = htons(PORT); 
-    servaddr.sin_addr.s_addr = INADDR_ANY; 
-      
-    int n, len; 
-      
-    sendto(sockfd, (const char *)hello, strlen(hello), 
-        MSG_CONFIRM, (const struct sockaddr *) &servaddr,  
-            sizeof(servaddr)); 
-    printf("Hello message sent.\n"); 
-          
-    n = recvfrom(sockfd, (char *)buffer, MAXLINE,  
-                MSG_WAITALL, (struct sockaddr *) &servaddr, 
-                &len); 
-    buffer[n] = '\0'; 
-    printf("Server : %s\n", buffer); 
-  
-    close(sockfd); 
-    return 0; 
+/* Nickolas Gough, nvg081, 11181823 */
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+
+#include "x-sockets.h"
+#include "x-common.h"
+
+
+int main(int argc, char* argv[]) {
+    char* message;
+    char* sName;
+    char* sPort;
+    int serverFd;
+    struct addrinfo serverInfo;
+
+    if (argc != 3) {
+        printf("usage: ./x-client <host name> <port number>\n");
+        exit(1);
+    }
+
+    sName = argv[1];
+    sPort = argv[2];
+    if (!check_port(sPort)) {
+        printf("x-client: port number must be between 30000 and 40000\n");
+        exit(1);
+    }
+
+    if (!udp_socket(&serverFd, &serverInfo, sName, sPort)) {
+        printf("x-client: failed to create udp socket for given host\n");
+        exit(1);
+    }
+
+    message = calloc(INPUT_MAX, sizeof(char));
+    if (message == NULL) {
+        printf("x-client: failed to allocate necessary memory\n");
+        exit(1);
+    }
+
+    if (sendto(serverFd, "hello", INPUT_MAX, 0, (struct sockaddr*) serverInfo.ai_addr, serverInfo.ai_addrlen) == -1) {
+        printf("x-client: failed to send to server\n");
+        exit(1);
+    }
+    if (recvfrom(serverFd, message, INPUT_MAX, 0, (struct sockaddr*) NULL, NULL) == -1) {
+        printf("x-client: failed to receive from server\n");
+        exit(1);
+    }
+    printf("%s\n", message);
+
+    close(serverFd);
+    exit(0);
 }
