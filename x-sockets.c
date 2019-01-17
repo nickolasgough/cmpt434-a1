db6a46ea8647f3a8761fa02ce4a12dd67a020cb5
Nickolas Gough <nickolas.v.gough@gmail.com>
2019-01-17 16:06:22 -0600

    Try fixing it.

--- x-sockets.c
+++ x-sockets.c
@@ -29,15 +29,13 @@ int tcp_socket(int* outFd, struct addrinfo** outInfo, char* mName, char* port) {
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_protocol = IPPROTO_TCP;
-    if (mName == NULL) {
-        hints.ai_flags = AI_PASSIVE;
-    }
+    hints.ai_flags = AI_PASSIVE;
     sprintf(hName, "%s.usask.ca", mName);
     
     if (getaddrinfo(hName, port, &hints, &pAi) != 0) {
         return 0;
     }
-    if (pAi->ai_family != AF_INET || pAi->ai_socktype != SOCK_STREAM || pAi->ai_protocol != IPPROTO_TCP) {
+    if (pAi->ai_socktype != SOCK_STREAM || pAi->ai_protocol != IPPROTO_TCP) {
         return 0;
     }
     
@@ -67,15 +65,13 @@ int udp_socket(int* outFd, struct addrinfo** outInfo, char* mName, char* port) {
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_DGRAM;
     hints.ai_protocol = IPPROTO_UDP;
-    if (mName == NULL) {
-        hints.ai_flags = AI_PASSIVE;
-    }
+    hints.ai_flags = AI_PASSIVE;
     sprintf(hName, "%s.usask.ca", mName);
     
     if (getaddrinfo(hName, port, &hints, &pAi) != 0) {
         return 0;
     }
-    if (pAi->ai_family != AF_INET || pAi->ai_socktype != SOCK_DGRAM || pAi->ai_protocol != IPPROTO_UDP) {
+    if (pAi->ai_socktype != SOCK_DGRAM || pAi->ai_protocol != IPPROTO_UDP) {
         return 0;
     }
     
