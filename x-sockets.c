ad24dfa4a941b69596c0d3bdc1297971edc57619
Nickolas Gough <nickolas.v.gough@gmail.com>
2019-01-17 14:45:56 -0600

    Try something.

--- x-sockets.c
+++ x-sockets.c
@@ -34,7 +34,7 @@ int tcp_socket(int* outFd, struct addrinfo* outInfo, char* mName, char* port) {
     if (getaddrinfo(hName, port, &hints, &pAi) != 0) {
         return 0;
     }
-    if (pAi->ai_family != AF_INET || pAi->ai_socktype != SOCK_STREAM) {
+    if (pAi->ai_socktype != SOCK_STREAM || pAi->ai_protocol != IPPROTO_TCP) {
         return 0;
     }
     
@@ -61,7 +61,7 @@ int udp_socket(int* outFd, struct addrinfo* outInfo, char* mName, char* port) {
     }
     
     memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_INET;
+    hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_DGRAM;
     hints.ai_protocol = IPPROTO_UDP;
     hints.ai_flags = AI_PASSIVE;
@@ -70,7 +70,7 @@ int udp_socket(int* outFd, struct addrinfo* outInfo, char* mName, char* port) {
     if (getaddrinfo(hName, port, &hints, &pAi) != 0) {
         return 0;
     }
-    if (pAi->ai_family != AF_INET || pAi->ai_socktype != SOCK_DGRAM || pAi->ai_protocol != IPPROTO_UDP) {
+    if (pAi->ai_socktype != SOCK_DGRAM || pAi->ai_protocol != IPPROTO_UDP) {
         return 0;
     }
     
